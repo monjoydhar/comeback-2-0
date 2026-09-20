912acaf47ba71581c1e944fed8601844a0d74bb3
MONJOY <monjoy@Monjoys-MacBook-Air.local>
2026-09-20 12:54:14 +0600

    fix pdf on vercel, prisma generate, daily cron

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,14 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = { reactStrictMode:true };
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  serverExternalPackages: ["pdfkit"],
+
+ 
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/pdfkit/**/*"],
+  },
+};
+
 export default nextConfig;
